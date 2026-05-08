71695faf7ed41f0bfc4e0193c1c7ed5f8f2845f1
Jamison Lahman <jamison@lahman.dev>
2026-05-08 12:53:23 -0700

    nit

--- web/src/components/SessionPane.tsx
+++ web/src/components/SessionPane.tsx
@@ -216,4 +216,3 @@ export function SessionPane({
     </aside>
   );
 }
-
--- web/src/components/TerminalsRoot.tsx
+++ web/src/components/TerminalsRoot.tsx
@@ -111,4 +111,3 @@ function sameIds(a: number[], b: number[]): boolean {
   for (let i = 0; i < a.length; i++) if (a[i] !== b[i]) return false;
   return true;
 }
-
